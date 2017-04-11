468f403ceb3aa965a945ecfbcadbb054c6740497
Jason Steele <jason.omnibus@gmail.com>
2017-04-11 14:22:51 -0400

    MetaMask integration working for testnet

--- client/js/actionCreators/userActionCreators.js
+++ client/js/actionCreators/userActionCreators.js
@@ -2,11 +2,16 @@ import userActions from '../actions/userActions';
 const w3 = require('Web3');
 const provider_endpoint = process.argv[2] ? process.argv[2].substr(2) : 'http://localhost:8545';
 let web3;
-if (typeof web3 !== 'undefined') {
-  web3 = new w3(web3.currentProvider);
+if (typeof window.web3 !== 'undefined') {
+  web3 = new w3(window.web3.currentProvider);
 } else {
   web3 = new w3(new w3.providers.HttpProvider(provider_endpoint));
 }
+// if (typeof web3 !== 'undefined') {
+//   web3 = new w3(web3.currentProvider);
+// } else {
+//   web3 = new w3(new w3.providers.HttpProvider(provider_endpoint));
+// }
 
 export function setAccount() {
     return {
